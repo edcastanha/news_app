991fb7f22b59751faad54f4b5a87fe2a4531a714
Edson Lourenço <edcastanha@gmail.com>
2021-03-03 14:55:06 -0300

    Validado rotas e trabalhado questões de erro por case sensitive e encaminhamento para caso não exista o controler para erroContrroler

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
 
-/*# Verificação de controle de rota
-/## Caso a view não seja encontrada retorna para index
-/
-/
+/*# Verificação de controle por rota
+/## Caso a view não seja encontrada retorna para index por padrão
 */
 if(isset($_GET['view'])){
-    if(files_exits('core/controles/'.strtolower($_GET['view'])));
+    //para evitar erro por despadronização de case sensitive nas rotas utilizarei a função php strTolower
+    if(file_exists('core/controles/'.strtolower($_GET['view'].'Controler.php'))){
+        /*a variavel "views" sendo varida incluirá o contrle equivalente 
+        (exemplo: ?view=autores => /autoresControler.php será apresentado)
+        */
+        include('core/controles/'.strtolower($_GET['view'].'Controler.php'));
+    }else{
+         //Rota padrão para erro de rota
+    include('core/controles/erroControler.php');
+    }
+        
+    
 
 }else{
+    //Rota padrão
     include('core/controles/indexControler.php');
 } 
